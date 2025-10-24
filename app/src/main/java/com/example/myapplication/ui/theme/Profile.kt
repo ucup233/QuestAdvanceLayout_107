0b5e24e78455be4f30a37d064ad97dd57a456d95
ucup233 <sufhidayat9@gmail.com>
2025-10-24 19:20:27 +0700

    Pembuatan Card pertama

--- app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
@@ -7,19 +7,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
@@ -90,7 +99,55 @@ fun HomePage(modifier: Modifier){
         )
         Text(
             text = stringResource(id = R.string.description),
-            fontSize = 19.sp
+            fontSize = 19.sp,
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.Cursive,
+            modifier = Modifier
+                .padding(top = 5.dp)
         )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ){
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically)
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.tiktok),
+                    contentDescription = null,
+
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(
+                    modifier = Modifier.width(width = 20.dp)
+                )
+                Text(
+                    text = stringResource(R.string.username),
+                    fontSize = 23.sp,
+                    color = White
+                    )
+                Spacer(
+                    modifier = Modifier.width(width = 50.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.go),
+                    contentDescription = null,
+
+                    modifier = Modifier
+                        .size(size = 25.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+
+            }
+        }
     }
 }
