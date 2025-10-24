74a8695402f0a609641d129ab49d4e85630af743
ucup233 <sufhidayat9@gmail.com>
2025-10-24 09:13:06 +0700

    Pembuatan Text pertama dalam column

--- app/src/main/java/com/example/myapplication/UiPertama.kt
+++ app/src/main/java/com/example/myapplication/UiPertama.kt
@@ -3,8 +3,13 @@ package com.example.myapplication
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
@@ -12,4 +17,10 @@ fun ActivitasPertama(modifier: Modifier){
         .padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = stringResource(id= R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
