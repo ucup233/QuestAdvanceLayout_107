9ad94d215c6251d838c0ab192d4bf73aeecddb55
ucup233 <sufhidayat9@gmail.com>
2025-10-24 09:11:37 +0700

    Inisialisasi Function ActivitasPertama dengan modif padding, max size, dan alignment

---
+++ app/src/main/java/com/example/myapplication/UiPertama.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier
+        .padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
