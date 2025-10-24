baab708d2d844140d59780cbcc14edc775dfe8dc
ucup233 <sufhidayat9@gmail.com>
2025-10-24 20:05:26 +0700

    Pembuatan card ketiga

--- app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
@@ -172,9 +172,46 @@ fun HomePage(modifier: Modifier) {
                 )
             }
         }
-
-
-
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.DarkGray)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.gmail),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape),
+                    contentScale = androidx.compose.ui.layout.ContentScale.Crop
+                )
+                Text(
+                    text = stringResource(R.string.email),
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 10.dp),
+                    maxLines = 1
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.go),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clip(CircleShape),
+                    contentScale = androidx.compose.ui.layout.ContentScale.Crop
+                )
+            }
+        }
 
     }
 }
