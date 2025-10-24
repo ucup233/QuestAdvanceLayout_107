5eb74a9114915c564b22b7d6d67453c8f11fb415
ucup233 <sufhidayat9@gmail.com>
2025-10-24 09:14:00 +0700

    Pembuatan Text kedua "univ" dalam column

--- app/src/main/java/com/example/myapplication/UiPertama.kt
+++ app/src/main/java/com/example/myapplication/UiPertama.kt
@@ -23,4 +23,9 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
+        Text(
+                text = stringResource(id = R.string.univ),
+        fontSize = 22.sp
+        )
+
     }
