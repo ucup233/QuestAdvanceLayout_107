ef31a9a8924e01b1e5da68f48872a6915e35bd19
ucup233 <sufhidayat9@gmail.com>
2025-10-24 16:08:00 +0700

    Pembuatan fungsi HomePage

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.HomePage
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ActivitasPertama(modifier = Modifier
+                    HomePage(modifier = Modifier
                         .padding(innerPadding))
                 }
             }
---
+++ app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+
+@Composable
+fun HomePage(modifier: Modifier){
+    Column(modifier = Modifier
+        .padding(top = 70.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
