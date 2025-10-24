5bb4c13f902698d28414ca97f2dfc9ac313ed3ca
ucup233 <sufhidayat9@gmail.com>
2025-10-24 16:56:08 +0700

    Pembuatan Nama dengan menggunakan elemen "namaku"

--- app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Profile.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
 
 @Composable
@@ -70,5 +77,13 @@ fun HomePage(modifier: Modifier){
                 contentScale = ContentScale.Crop
             )
         }
+        Text(
+            text = stringResource(id= R.string.namaku),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 10.dp)
+        )
+
     }
 }
