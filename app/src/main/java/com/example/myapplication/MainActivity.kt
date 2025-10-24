12a1c60a3e5dbda434b330c5533bacc87e908854
ucup233 <sufhidayat9@gmail.com>
2025-10-24 10:04:59 +0700

    Pemanggilan fungsi di app utama

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,10 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ActivitasPertama(modifier = Modifier
+                        .padding(innerPadding))
                 }
             }
         }
